05c0d9f8260fb09e3d72f96b977c4510f2e3f774
mihirbd <mstarequlislam@gmail.com>
2022-07-23 23:07:44 +0600

    angular add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+export interface Information {
+  name: String;
+  email: String;
+  password: String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'TemplateDrivenForm';
+
+  information: Information = {
+    name: '',
+    email: '',
+    password: ''
+  }
+  constructor() {
+    this.information = {} as Information;
+  }
+
+  formSubmit(value: any) {
+    console.log(value)
+    console.log("Form Submited Successfully")
+  }
+
 }
